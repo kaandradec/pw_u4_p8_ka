f411fda3f9f106621efc607c3eff4266b85725ac
kaandradec <kaandradec@gmail.com>
2025-07-18 19:26:27 -0500

    taller 39: JWT

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <!-- <NavBar></NavBar> -->
+   <Login/>
   <router-view />
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
+import Login from "./components/Login.vue";
 export default {
   name: "App",
   components: {
-    NavBar,
+    // NavBar,
+    Login
   },
 };
 </script>
--- src/clients/EstudianteClient.js
+++ src/clients/EstudianteClient.js
@@ -4,9 +4,14 @@ import axios from "axios";
 // public Response guardar(@RequestBody EstudianteTo estudianteTo) {
 
 const URL_API = 'http://localhost:8081/api/matricula/v1/estudiantes';
+let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwOi8vdWNlLmVkdS5lYyIsInVwbiI6Im1pY29ycmVvQHVjZS5lZHUuZWMiLCJncm91cHMiOlsiYWRtaW5zIl0sImlhdCI6MTc1Mjg4NDY3MiwiZXhwIjoxNzUyODg0NzAyLCJqdGkiOiJkNzdlOWFjMy05YTVkLTRlMWUtYmQzNy1lNGVjNjhlOWI5MDUifQ.q1mcGn2J0TvOuod8_Eu2mXULmPlobNPwHXxBX7naqqAft5CX2SCu9ElEXOQL75x-lbxIlXN8LS_vt6LD0qBsqkWbULH6ye4AnyFJY4uIyxsZEUKw8aZ5z2MptMA9Yn82b-rdjPnaV6tdQClmnSmc_6qoyrhFqZQDPUomPx9Lm-7SU-YlMwyYUB26ByOC7Ea0HnteNawrc9_6Su0k1qdcw_K12sFfQa7aHorv_3uSyn-Ru2vvwnKT3LhttTocXZ9s8u4FPlI7yvOgdB2RpSg-RT7LYnl6x87uUd0Bv85wxZd-5ix6Z0WqZHccOk8XmH3L5z6lGHrHyjAO4FS3UXbXqw";
 
 const guardar = async (body) => {
-  const data = axios.post(URL_API, body).then(res => res.data);
+  const data = axios.post(URL_API, body, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }).then(res => res.data);
   console.log(data);
 }
 
